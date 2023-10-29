bc072aa315acc9d9f5ea1e7d8eaf707782ff632d
Felipe Galdino <felipebatistagaldino.pessoal@gmail.com>
2023-10-29 12:10:11 -0300

    Starting to implement Board and Piece

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,9 @@
 package application;
 
+import boardgame.Board;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Board board = new Board(8, 8);
     }
 }
